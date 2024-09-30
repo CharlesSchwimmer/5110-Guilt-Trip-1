550f8fd7da41987724db85d168a8e79c9bc57740
TongXiao2000 <xt092218@gmail.com>
2024-09-30 15:05:18 -0400

    create credit scene

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
     public TMP_Text[] Tasks;
+
+    public void ShowCredit(){
+        SceneManager.LoadScene("Credit");
+    }
 
 
     //GetComponent<TextMeshProUGUI>().text = "Day: " + dayTracker.ToString() + " / 3";
